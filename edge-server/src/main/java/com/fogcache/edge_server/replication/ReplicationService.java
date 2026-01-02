c34b55545b9cd2d4a437692281c9094086055527
prateek khurmi <115011912+Pki03@users.noreply.github.com>
2026-01-02 17:15:34 +0530

    Day 10: Implement fault tolerance, health checks, crash recovery & cache warmup for self-healing edge network

--- edge-server/src/main/java/com/fogcache/edge_server/replication/ReplicationService.java
+++ edge-server/src/main/java/com/fogcache/edge_server/replication/ReplicationService.java
@@ -4,30 +4,48 @@ import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
 
 import java.util.List;
-import java.util.concurrent.ExecutorService;
-import java.util.concurrent.Executors;
+import java.util.Map;
 
 @Service
 public class ReplicationService {
 
-    private final ExecutorService executor = Executors.newFixedThreadPool(4);
     private final RestTemplate restTemplate = new RestTemplate();
 
-    // Hardcode edges for now
     private final List<String> edgeNodes = List.of(
             "http://localhost:8082",
             "http://localhost:8083"
     );
 
-    public void replicate(String currentNode, String key, String value) {
+    public void replicate(String source, String key, String value) {
         for (String node : edgeNodes) {
-            if (!node.equals(currentNode)) {
-                executor.submit(() -> {
-                    try {
-                        ReplicationRequest req = new ReplicationRequest(key, value);
-                        restTemplate.postForObject(node + "/replicate", req, String.class);
-                    } catch (Exception ignored) {}
-                });
+            if (!node.equals(source)) {
+                try {
+                    restTemplate.postForObject(
+                            node + "/replicate",
+                            new ReplicationRequest(key, value),
+                            String.class
+                    );
+                } catch (Exception ignored) {
+                }
+            }
+        }
+    }
+
+    public void syncNode(String recoveringNode) {
+        for (String node : edgeNodes) {
+            if (!node.equals(recoveringNode)) {
+                try {
+                    Map<String, String> data =
+                            restTemplate.getForObject(node + "/dump", Map.class);
+
+                    restTemplate.postForObject(
+                            recoveringNode + "/warmup",
+                            data,
+                            Void.class
+                    );
+                    break;
+                } catch (Exception ignored) {
+                }
             }
         }
     }
